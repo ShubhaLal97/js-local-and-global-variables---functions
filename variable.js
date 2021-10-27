570cffeee16c5eb3ed63917285d5c1de02bff0a4
shubha <shubha07658@gmail.com>
2021-10-27 21:53:55 +0530

    variables

---
+++ variable.js
@@ -0,0 +1,26 @@
+//var num1 = 10 // num1 is a global variable
+
+//function sum(num2) {
+    //var total = num1 + num2 // total is a local variable
+    //var total = 20
+
+    //console.log(total) // local scope of num1
+//}
+
+//function mul(num2) {
+    //var total = num1 * num2
+    //console.log(total)
+//}
+
+
+//console.log(num1) // global scope of num1
+//console.log(total1)
+
+var num1 = 24
+
+function numdiff() {
+    var num2 = 20
+    console.log(num1 - num2)
+}
+console.log(numdiff)
+numdiff()
